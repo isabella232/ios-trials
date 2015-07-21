f3d340892441e751eb7c8c2690a07fd22edfaf09
Patrick Debois <Patrick.Debois@jedi.be>
2015-07-21 14:48:47 +0200

    Initial Commit

---
+++ trials1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  trials1
+//
+//  Created by patrick debois on 21/07/15.
+//  Copyright (c) 2015 patrick debois. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
